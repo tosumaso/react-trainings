c1304c0defe5ff9e3cb10a25fe8574811703c7fe
tosumaso <wakuwaku5712@gmail.com>
2022-04-08 18:13:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//BrowserRouter: <BrowserRouter>で囲った配下でルーティングを有効化する
+//Link: aタグのようにlinkを意味する
+//Switch: Linkと遷移先の紐付けを行う設定。<Switch>内にルーティングの設定を記述する
+//Route: どのpathにどのコンポーネントを出力するか
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
-function App() {
+import './App.css';
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/page1">Page1</Link>
+        <Link to="/page2">Page2</Link>
+      </div>
+      {/** exact: pathに指定したurlが完全一致した場合のみRoute内のコンポーネントを呼ぶ
+       *  exactがないとpath="/"の場合"/"配下のurl全てが読み込み対象になってしまう
+       */}
+      <Switch>
+      <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/page1">
+          <Page1 />
+        </Route>
+        <Route exact path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>Page1ページ</h1>
+    </div>
+  )
+}
